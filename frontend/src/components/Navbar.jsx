91fd626ffaa28ec31a7dc18895c3299726da4655
Murari <murarithakur32@gmail.com>
2026-01-03 01:14:01 +0530

    login function using clerk

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { use } from "react";
 import { assets } from "../assets/assets";
-import { Link } from "react-router-dom";
-import { MenuIcon, SearchIcon, XIcon } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { MenuIcon, SearchIcon, TicketIcon, XIcon } from "lucide-react";
 import { useState } from "react";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
+  const { user } = useUser();
+  const { openSignIn } = useClerk();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <div className="fixed flex top-0 left-0 z-50 w-full items-center justify-between px-6 py-5 md:px-16 lg:px-16">
       <Link to="/" className="max-md:flex-1">
@@ -64,9 +68,25 @@ const Navbar = () => {
       </div>
       <div className="flex items-center gap-8">
         <SearchIcon className="max-md:hidden w-6 h-6 cursor-pointer" />
-        <button className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer">
-          Login
-        </button>
+
+        {!user ? (
+          <button
+            onClick={openSignIn}
+            className="px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer"
+          >
+            Login
+          </button>
+        ) : (
+          <UserButton>
+            <UserButton.MenuItems>
+              <UserButton.Action
+                label="My Booking"
+                labelIcon={<TicketIcon width={15} />}
+                onClick={() => navigate("/my-bookings")}
+              ></UserButton.Action>
+            </UserButton.MenuItems>
+          </UserButton>
+        )}
       </div>
       <MenuIcon
         className="max-md:ml-4 md:hidden w-8 h-8 cursor-pointer"

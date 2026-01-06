00005b811adb981ac8ac477ef62f32ac736afaad
Murari <murarithakur32@gmail.com>
2026-01-06 17:29:52 +0530

    final fix

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -8,12 +8,12 @@ const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_adresses, image_url } =
-      event.data.user;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
       _id: id,
-      name: first_name + "" + last_name,
-      email: email_adresses[0].email_address,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
       image: image_url,
     };
     await user.create(userData);
@@ -33,12 +33,12 @@ const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_adresses, image_url } =
-      event.data.user;
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
     const userData = {
       _id: id,
-      name: first_name + "" + last_name,
-      email: email_adresses[0].email_address,
+      name: first_name + " " + last_name,
+      email: email_addresses[0].email_address,
       image: image_url,
     };
     await user.findByIdAndUpdate(id, userData);
